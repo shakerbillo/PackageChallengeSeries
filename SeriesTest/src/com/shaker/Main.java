8fd18b65f1182b9859ee3dce7bc97c31194a5aba
Emmanuel Gyan-Amoah <82492796+shakerbillo@users.noreply.github.com>
2022-01-24 08:45:26 +0100

    Add files via upload

---
+++ SeriesTest/src/com/shaker/Main.java
@@ -0,0 +1,23 @@
+package com.shaker;
+
+import com.shaker.mylibrary.Series;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+            for(int i=0; i<=10; i++){
+                System.out.println(Series.nSum(i));
+            }
+            System.out.println("**********************************");
+            for(int i=0; i<=10; i++){
+                System.out.println(Series.factorial(i));
+            }
+            System.out.println("**********************************");
+        for(int i=0; i<=10; i++){
+            System.out.println(Series.fibonacci(i));
+        }
+    }
+}
+
+
